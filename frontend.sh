f946c30ddf0b5154e4e67d04bdf5b3a995dec02b
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-19 09:38:23 +0530

    shipping

--- frontend.sh
+++ frontend.sh
@@ -1,4 +1,5 @@
 yum install nginx -y
+systemctl start nginx
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
@@ -6,3 +7,4 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 systemctl restart nginx
 systemctl enable nginx
+
