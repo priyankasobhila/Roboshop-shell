3b99f47218a1e16cb43f9fcfee2ea7facb228149
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-20 19:13:16 +0530

    user echo changes

---
+++ user.sh
@@ -0,0 +1,40 @@
+echo -e "\e[34m>>>>>>>>> download nodejs <<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[34m>>>>>>>>> install nodejs <<<<<<<<\e[0m"
+yum install nodejs -y
+
+echo -e "\e[32m>>>>>>>>> add application user <<<<<<<<\e[0m"
+useradd roboshop
+
+echo -e "\e[34m>>>>>>>>> create app directory <<<<<<<<\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[34m>>>>>>>>> download app content <<<<<<<<\e[0m"
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+
+echo -e "\e[34m>>>>>>>>> unzip app content <<<<<<<<\e[0m"
+unzip /tmp/user.zip
+cd /app
+
+echo -e "\e[34m>>>>>>>>> install nodejs dependencies <<<<<<<<\e[0m"
+npm install
+
+echo -e "\e[34m>>>>>>>>> copy catalogue systemd file <<<<<<<<\e[0m"
+cp user.service /etc/systemd/system/user.service
+
+echo -e "\e[32m>>>>>>>>> start catalogue service<<<<<<<<\e[0m"
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
+
+echo -e "\e[32m>>>>>>>>> copy mongod repo <<<<<<<<\e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[32m>>>>>>>>> install mongod client <<<<<<<<\e[0m"
+yum install mongodb-org-shell -y
+
+echo -e "\e[32m>>>>>>>>> load schema <<<<<<<<\e[0m"
+mongo --host mongodb.priyavenkat.online /app/schema/user.js
