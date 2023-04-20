f4f74b43c417a47a94423e11e3dfd5b11a289187
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-20 19:21:51 +0530

    user echo changes

--- user.sh
+++ user.sh
@@ -37,4 +37,4 @@ echo -e "\e[32m>>>>>>>>> install mongod client <<<<<<<<\e[0m"
 yum install mongodb-org-shell -y
 
 echo -e "\e[32m>>>>>>>>> load schema <<<<<<<<\e[0m"
-mongo --host mongodb.priyavenkat.online /app/schema/user.js
+mongo --host mongodb-dev.priyavenkat.online /app/schema/user.js
