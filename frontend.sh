73f5d110e5e917dc9585ff47c03db49d8d20d3e2
sobhila.venkataramana <sobhila.venkataramana@gmail.com>
2023-04-21 12:24:17 +0530

    cart  changes

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,25 @@
+echo -e "\e[31m>>>>>>>>> install python36 <<<<<<<<\e[0m"
 yum install nginx -y
+
+echo -e "\e[31m>>>>>>>>> start the service <<<<<<<<\e[0m"
 systemctl start nginx
+
+echo -e "\e[31m>>>>>>>>> copy the configuration file <<<<<<<<\e[0m"
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[31m>>>>>>>>> remove the configuration file <<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[31m>>>>>>>>> download the content <<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+
+echo -e "\e[31m>>>>>>>>> copy the roboshop configuration file <<<<<<<<\e[0m"
+cp /home/centos/Roboshop-shell/roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[31m>>>>>>>>> extract the content <<<<<<<<\e[0m"
 unzip /tmp/frontend.zip
+
+echo -e "\e[31m>>>>>>>>> restart the service <<<<<<<<\e[0m"
 systemctl restart nginx
 systemctl enable nginx
 
